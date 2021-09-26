d9ac00f980dce232f9191fd52d795d36557a5c97
Robbie Van Gorkom <robbie.van.gorkom@protonmail.com>
2021-09-26 13:02:05 -0700

    Add logging. Code cleanup.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use std::collections::HashMap;
 use std::fs;
+use std::io::Write;
 use std::process::exit;
 use std::str;
 
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, web};
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, middleware::Logger, web};
+use chrono::Local;
 use clap;
 use clap::{AppSettings, Clap};
 use curl::easy::Easy;
+use env_logger::Builder;
 use jq_rs;
+use log::LevelFilter;
 use serde::Deserialize;
 use serde::Serialize;
 use serde_yaml;
@@ -48,105 +52,116 @@ struct CliArgs {
     config_file_path: String,
 }
 
-async fn proxy(req: HttpRequest, data: web::Data<AppConfig>) -> HttpResponse {
-    if !data.paths.contains_key(req.path()) {
-        return HttpResponse::NotFound()
-            .json(ErrorResponse {
-                is_error: true,
-                message: format!("Path not configured")
-            });
-    }
-
-    let path_config = data.paths.get(req.path()).unwrap().clone();
+fn do_curl_and_jq(path_config: PathConfig) -> Result<String, String> {
     let mut easy = Easy::new();
 
-    let url_result = easy.url(&path_config.source_url);
-    if url_result.is_err() {
-        return HttpResponse::InternalServerError()
-            .json(ErrorResponse {
-                is_error: true,
-                message: format!("Failed to retrieve from URL: {}", url_result.unwrap_err()),
-            })
-    }
+    // Set the URL.
+    easy.url(&path_config.source_url).unwrap();
+    easy.fail_on_error(true).unwrap();
 
+    // Curl for the data.
     let mut body_string = String::new();
     {
         let mut transfer = easy.transfer();
-        transfer.write_function(|data| {
-            body_string.push_str(str::from_utf8(data).unwrap());
-            Ok(data.len())
-        }).unwrap();
+        transfer
+            .write_function(|data| {
+                body_string.push_str(str::from_utf8(data).unwrap());
+                Ok(data.len())
+            })
+            .unwrap(); // Always returns Ok()
 
-        let transfer_perform_result = transfer.perform();
-        if transfer_perform_result.is_err() {
-            return HttpResponse::BadRequest()
-                .json(ErrorResponse {
-                    is_error: true,
-                    message: format!("{}", transfer_perform_result.unwrap_err())
-                })
-        }
+        transfer.perform()
+            .map_err(|e| format!("Failed to run curl: {}", e))?;
     }
 
-    let jq_result = jq_rs::run(&path_config.jq_filter, &body_string);
-    if jq_result.is_err() {
-        return HttpResponse::InternalServerError()
+    // Query the data.
+    return Ok(jq_rs::run(&path_config.jq_filter, &body_string)
+        .map_err(|e| format!("Failed to run jq: {}", e))?
+    );
+}
+
+fn proxy(req: HttpRequest, data: web::Data<AppConfig>) -> HttpResponse {
+    // Path will exist as the only paths configured are based on the app context data.
+    let path_config = data.paths.get(req.path())
+        .unwrap()
+        .clone();
+
+    // HttpResponse::new()
+    return match do_curl_and_jq(path_config) {
+        Ok(data) => HttpResponse::Ok()
+            .content_type("application/json")
+            .body(data),
+        Err(message) => HttpResponse::InternalServerError()
             .json(ErrorResponse {
                 is_error: true,
-                message: format!("Failed to run JQ: {}", jq_result.unwrap()),
+                message,
             })
     }
+}
 
-    return HttpResponse::Ok()
-        .content_type("application/json")
-        .body(jq_result.unwrap())
+fn parse_config(config_file_path: String) -> Result<AppConfig, String> {
+    log::debug!("Reading config file {}.", config_file_path);
+    let config_file_contents = fs::read_to_string(config_file_path)
+        .map_err(|e| format!("Config read error: {}", e))?;
+
+    log::debug!("Parse config file.");
+    let config: AppConfig = serde_yaml::from_str(&config_file_contents)
+        .map_err(|e| format!("Config parse error: {}", e))?;
+
+    return match config.paths.is_empty() {
+        true => Err("No paths configured in config file.".to_string()),
+        false => Ok(config),
+    };
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Configure logging output.
+    Builder::new()
+        .format(|buf, record| {
+            writeln!(buf,
+                     "{} [{}] - {}",
+                     Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                     record.level(),
+                     record.args()
+            )
+        })
+        .filter(None, LevelFilter::Info)
+        .init();
 
+    log::debug!("Parsing CLI args.");
     let cli_args: CliArgs = CliArgs::parse();
 
-    let config_file_contents_result = fs::read_to_string(cli_args.config_file_path);
-    if config_file_contents_result.is_err() {
-        eprintln!("Config read error: {}", config_file_contents_result.unwrap_err());
-        exit(1);
-    }
-
-    let config_result: Result<AppConfig, serde_yaml::Error> = serde_yaml::from_str(config_file_contents_result.unwrap().as_str());
+    let config_result = parse_config(cli_args.config_file_path);
     if config_result.is_err() {
-        eprintln!("Config parse error: {}", config_result.unwrap_err());
+        eprintln!("{}", config_result.unwrap_err());
         exit(1);
     }
 
-    let config: AppConfig = config_result.unwrap();
-    let listen = config.listen.clone();
+    let config = config_result.unwrap();
 
-    if config.paths.len() == 0 {
-        eprintln!("No paths configured.");
-        exit(1)
-    }
-
-    println!("Listening on address {}", listen);
+    println!("Configured paths:");
     (&config.paths).into_iter().for_each(|item| {
         println!("  {} -> {}", item.0, item.1.source_url);
     });
 
+    let moved_config = config.clone();
     HttpServer
         ::new(move || {
-            let config_clone = config.clone();
-            return App::new()
-                .app_data(web::Data::new(config.clone()))
-                .configure(|cfg: &mut web::ServiceConfig| {
-                    config_clone.paths.into_iter().for_each(|item| {
-                        let path = item.0;
+            let mut app = App::new()
+                .wrap(Logger::new("%r"))
+                .app_data(web::Data::new(moved_config.clone()));
 
-                        cfg.service(
-                            web::resource(path).route(web::get().to(proxy))
-                        );
-                    });
-                });
+            // Configure the paths.
+            for (path, _path_config) in &(moved_config.paths) {
+                app = app.service(web::resource(path).route(
+                    web::get().to(proxy)
+                ));
+            }
+
+            return app;
         })
-        .bind(listen)?
+        .bind(config.listen)?
         .run()
         .await
 }
